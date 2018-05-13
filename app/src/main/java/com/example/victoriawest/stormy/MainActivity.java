488bdb498611eb588c40575db7e18473e99e1408
Victoria West <victoriawest811@gmail.com>
2018-05-13 15:07:48 -0400

    somethings not running this through, ill need help debugging this stuff here

--- app/src/main/java/com/example/victoriawest/stormy/MainActivity.java
+++ app/src/main/java/com/example/victoriawest/stormy/MainActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import java.io.IOException;
 
 import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -30,15 +31,25 @@ public class MainActivity extends AppCompatActivity {
 
         Request request = new Request.Builder().url(forecastURL).build();
         Call call = client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
 
-        try {
-            Response response = call.execute();
-            if (response.isSuccessful()) {
-                Log.v(TAG, response.body().string());
             }
 
-        } catch (IOException e) {
-            Log.e(TAG, "IO Exception caught: ", e);
-        }
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+
+                    if (response.isSuccessful()) {
+                        Log.v(TAG, response.body().string());
+                    }
+
+                } catch (IOException e) {
+                    Log.e(TAG, "IO Exception caught: ", e);
+                }
+            }
+        });
+        Log.d(TAG, "main ui code is running");
     }
 }
