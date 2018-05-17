185660103ecc3a9014232c88a27991c300eb992a
Victoria West <victoriawest811@gmail.com>
2018-05-17 13:27:54 -0400

    added currentweather class and began setup for json data

--- app/src/main/java/com/example/victoriawest/stormy/MainActivity.java
+++ app/src/main/java/com/example/victoriawest/stormy/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.victoriawest.stormy;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 
@@ -16,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String TAG =MainActivity.class.getSimpleName();
 
+    private CurrentWeather currentWeather;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,35 +36,67 @@ public class MainActivity extends AppCompatActivity {
 
         String forecastURL = "https://api.darksky.net/forecast/" + apiKey + "/" + lattitude + "," + longitude;
 
-        OkHttpClient client = new OkHttpClient();
+        if (isNetworkAvailable()) {
+            OkHttpClient client = new OkHttpClient();
 
-        Request request = new Request.Builder()
-                .url(forecastURL)
-                .build();
+            Request request = new Request.Builder()
+                    .url(forecastURL)
+                    .build();
 
-        Call call = client.newCall(request);
-        call.enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-            }
-
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                try {
-                    Log.v(TAG, response.body().string());
-                    if (response.isSuccessful()) {
-
-                    } else {
-                        alertUserAboutError();
-                    }
-
-
-                } catch (IOException e) {
-                    Log.e(TAG, "IO Exception caught: ", e);
+            Call call = client.newCall(request);
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
                 }
-            }
-        });
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    try {
+                        String jsonData = response.body().string();
+                        Log.v(TAG, jsonData);
+                        if (response.isSuccessful()) {
+                            currentWeather = getCurrentDetails(jsonData);
+
+
+                        } else {
+                            alertUserAboutError();
+                        }
+
+
+                    } catch (IOException e) {
+                        Log.e(TAG, "IO Exception caught: ", e);
+                    }catch (JSONException e) {
+                        Log.e(TAG, "JSON error exception caught", e);
+
+                    }
+                }
+            });
+        }
         Log.d(TAG, "main ui code is running");
+    }
+
+    private CurrentWeather getCurrentDetails(String jsonData) throws JSONException {
+        JSONObject forecast = new JSONObject(jsonData);
+
+        String timezone = forecast.getString("timezone");
+        Log.i(TAG, "from JSON" + timezone);
+
+        return null;
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+
+        boolean isAvailable = false;
+        if (networkInfo != null && networkInfo.isConnected()) {
+            isAvailable = true;
+        }
+        else {
+            Toast.makeText(this, R.string.network_unavailable_message,Toast.LENGTH_LONG);
+        }
+
+        return isAvailable;
     }
 
     private void alertUserAboutError() {
