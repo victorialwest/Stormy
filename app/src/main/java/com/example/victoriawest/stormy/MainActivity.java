75ebb8f1da84c862307154ef294a5850004443cd
Victoria West <victoriawest811@gmail.com>
2018-05-18 15:19:43 -0400

    remade

--- app/src/main/java/com/example/victoriawest/stormy/MainActivity.java
+++ app/src/main/java/com/example/victoriawest/stormy/MainActivity.java
@@ -2,14 +2,19 @@ package com.example.victoriawest.stormy;
 
 import android.content.Context;
 import android.databinding.DataBindingUtil;
+import android.graphics.drawable.Drawable;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.example.victoriawest.stormy.databinding.ActivityMainBinding;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -21,26 +26,41 @@ import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import okhttp3.internal.tls.OkHostnameVerifier;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String TAG =MainActivity.class.getSimpleName();
-
+    public static final String TAG = MainActivity.class.getSimpleName();
     private CurrentWeather currentWeather;
+    private ImageView iconImageView;
+
+    double latitude = 37.8267;
+    double longitude = -122.4233;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        final ActivityMainBinding binding = DataBindingUtil.setContentView(MainActivity.this, R.layout.activity_main);
 
-        TextView darkSky = findViewById(R.id.darksky_attribution);
+        getForecast(latitude, longitude);
+        Log.d(TAG, "Main UI code is running, hooray!");
+    }
+
+    private void getForecast(double latitude, double longitude) {
+        final ActivityMainBinding binding = DataBindingUtil
+                .setContentView(MainActivity.this, R.layout.activity_main);
+
+        iconImageView = findViewById(R.id.iconImageView);
+
+        // Setup Dark Sky Link
+        TextView darkSky = findViewById(R.id.darkSkyAttribution);
         darkSky.setMovementMethod(LinkMovementMethod.getInstance());
 
         String apiKey = "80cc6a0ebfc0208808d0a822d4758789";
-        double lattitude = 37.8267;
-        double longitude = -122.4233;
 
-        String forecastURL = "https://api.darksky.net/forecast/" + apiKey + "/" + lattitude + "," + longitude;
+
+        String forecastURL = "https://api.darksky.net/forecast/" + apiKey +
+                "/" + latitude + "," + longitude;
 
         if (isNetworkAvailable()) {
             OkHttpClient client = new OkHttpClient();
@@ -53,55 +73,67 @@ public class MainActivity extends AppCompatActivity {
             call.enqueue(new Callback() {
                 @Override
                 public void onFailure(Call call, IOException e) {
+
                 }
 
                 @Override
-                public void onResponse(Call call, Response response) {
+                public void onResponse(Call call, Response response) throws IOException {
                     try {
                         String jsonData = response.body().string();
                         Log.v(TAG, jsonData);
                         if (response.isSuccessful()) {
                             currentWeather = getCurrentDetails(jsonData);
 
-                            CurrentWeather displayWeather = new CurrentWeather(
-                                    currentWeather.getLocationLabel();
-                                    currentWeather.getIcon();
-                                    currentWeather.getTime();
-                                    currentWeather.getTemperature();
-                                    currentWeather.getHumidity();
-                                    currentWeather.getPrecipChance();
-                                    currentWeather.getSummary();
-                                    currentWeather.getTimeZone();
+                            final CurrentWeather displayWeather = new CurrentWeather(
+                                    currentWeather.getLocationLabel(),
+                                    currentWeather.getIcon(),
+                                    currentWeather.getTime(),
+                                    currentWeather.getTemperature(),
+                                    currentWeather.getHumidity(),
+                                    currentWeather.getPrecipChance(),
+                                    currentWeather.getSummary(),
+                                    currentWeather.getTimeZone()
                             );
 
                             binding.setWeather(displayWeather);
 
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Drawable drawable = getResources().getDrawable(displayWeather.getIconId());
+                                    iconImageView.setImageDrawable(drawable);
+                                }
+                            });
+
                         } else {
                             alertUserAboutError();
                         }
-
                     } catch (IOException e) {
                         Log.e(TAG, "IO Exception caught: ", e);
-                    }catch (JSONException e) {
-                        Log.e(TAG, "JSON error exception caught", e);
-
+                    } catch (JSONException e) {
+                        Log.e(TAG, "JSON Exception caught: ", e);
                     }
+
                 }
             });
         }
-        Log.d(TAG, "main ui code is running");
+        else {
+            Toast.makeText(this, R.string.network_unavailable_message,
+                    Toast.LENGTH_LONG).show();
+        }
     }
 
     private CurrentWeather getCurrentDetails(String jsonData) throws JSONException {
         JSONObject forecast = new JSONObject(jsonData);
 
         String timezone = forecast.getString("timezone");
-        Log.i(TAG, "from JSON" + timezone);
+        Log.i(TAG, "From JSON: " + timezone);
 
-       JSONObject currently = forecast.getJSONObject("currently");
+        JSONObject currently = forecast.getJSONObject("currently");
 
         CurrentWeather currentWeather = new CurrentWeather();
 
+        // Parse weather data from currently object
         currentWeather.setHumidity(currently.getDouble("humidity"));
         currentWeather.setTime(currently.getLong("time"));
         currentWeather.setIcon(currently.getString("icon"));
@@ -121,11 +153,9 @@ public class MainActivity extends AppCompatActivity {
         NetworkInfo networkInfo = manager.getActiveNetworkInfo();
 
         boolean isAvailable = false;
+
         if (networkInfo != null && networkInfo.isConnected()) {
             isAvailable = true;
-        }
-        else {
-            Toast.makeText(this, R.string.network_unavailable_message,Toast.LENGTH_LONG);
         }
 
         return isAvailable;
@@ -134,6 +164,11 @@ public class MainActivity extends AppCompatActivity {
     private void alertUserAboutError() {
         AlertDialogFragment dialog = new AlertDialogFragment();
         dialog.show(getFragmentManager(), "error_dialog");
-
     }
+
+    public void refreshOnClick(View view) {
+        getForecast(latitude, longitude);
+        Toast.makeText(this, "Refreshing data", Toast.LENGTH_LONG).show();
+    }
+
 }
